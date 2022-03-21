9e1c7d7866719ef58fdbd6b9bc57430d4bc3ac73
martindario-28 <a19310419@ceti.mx>
2022-03-20 19:37:28 -0600

    p3
    p3

---
+++ P3_IA.py
@@ -0,0 +1,17 @@
+string1 = "Dios es " + "amor"
+print(string1)
+
+string2 = "1 Juan "
+string3 = "4:8"
+string4 = string2 + string3
+print(string4)
+
+print(string1, "y yo lo sé")
+
+string5 = "Martín Darío"
+string6 = "Espinosa Muñoz"
+string7 = string5 + " " + string6
+print(string7)
+
+string8 = "34" + "25"
+print(string8)
