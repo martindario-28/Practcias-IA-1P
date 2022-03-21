4eb12894a9b4809adf23a60537b87178a62a2702
martindario-28 <a19310419@ceti.mx>
2022-03-20 19:38:43 -0600

    p6
    p6

---
+++ P6_IA.py
@@ -0,0 +1,14 @@
+suma = 20+23+44
+print(suma)
+
+resta = 20+23-130
+print(resta)
+
+multiplicacion = (20-15)*(23+151)
+print(multiplicacion)
+
+division = 5000/(230*2)
+print(division)
+
+operacion = 10/5+15-17
+print(operacion)
