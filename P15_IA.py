0d640dd18b583da6c8ec326b62d6520815b4911d
martindario-28 <a19310419@ceti.mx>
2022-03-20 19:42:01 -0600

    p15
    p15

---
+++ P15_IA.py
@@ -0,0 +1,5 @@
+colores = ['rojo','azul','verde','amarillo','marrón','lila','negro','rosa','blanco','naranja']
+colores.insert(6,"magenta")
+#print(colores)
+colores.insert(-1,"turquesa")
+print(colores)
