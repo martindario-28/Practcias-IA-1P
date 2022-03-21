15623b59085ddb1f8e8725679254f99cb3466cfa
martindario-28 <a19310419@ceti.mx>
2022-03-20 19:37:47 -0600

    p4
    p4

---
+++ P4_IA.py
@@ -0,0 +1,8 @@
+nombre = "enrique barros fernández".title()
+print(nombre)
+
+nombre1 = "Esta Es Una Frase Para Ser Formateada".lower()
+print (nombre1)
+
+nombre2 = "Esta Es Una Frase Para Ser Formateada".upper()
+print(nombre2)
