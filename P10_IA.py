bd46fa4249b4fc866d364c0c67f5f055c924138a
martindario-28 <a19310419@ceti.mx>
2022-03-20 19:40:10 -0600

    p10
    p10

---
+++ P10_IA.py
@@ -0,0 +1,6 @@
+colores = ['rojo','azul','verde','amarillo','marrón','lila','negro','rosa','blanco','naranja']
+print("el valor -1 de la lsita es: ", colores[-1])
+print("el valor -7 de la lsita es: ", colores[-7])
+print("el valor -5 de la lsita es: ", colores[-5])
+print("el valor -2 de la lsita es: ", colores[-2])
+print("el valor -10 de la lsita es: ", colores[-10])
