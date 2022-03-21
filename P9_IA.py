80595a466e7c83a2a8aa6bdccac341fe00663bda
martindario-28 <a19310419@ceti.mx>
2022-03-20 19:39:51 -0600

    p9
    p9

---
+++ P9_IA.py
@@ -0,0 +1,8 @@
+colores = ["rojo","azul","verde","amarillo","marrón","lila","negro","rosa"]
+print(colores[2])
+
+print("El color rojo está en el lugar número ", (colores.index("rojo"))+1, "de la lista")
+print("El color rosa está en el lugar número ", (colores.index("rosa"))+1, "de la lista")
+
+lista = ["tres","dos","cinco","cuatro","uno"]
+print(lista)
