2b6b73dde47f89fb86a1661fc31660d0715a1d6c
martindario-28 <a19310419@ceti.mx>
2022-03-20 19:41:16 -0600

    p13
    p13

---
+++ P13_IA.py
@@ -0,0 +1,6 @@
+colores = ['rojo','azul','verde','amarillo','marrón','lila','negro','rosa','blanco','naranja']
+c1 = colores.pop(1)
+c2 = colores.pop(7)
+colores2 = [c1,c2]
+print("Lista original sin los elementos nuevos\n", colores)
+print("Lista nueva conformada por los elementos eliminados\n",colores2)
